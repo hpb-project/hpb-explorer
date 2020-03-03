dfbeb6fb08156c7bbc660fab686fe8cd27904119
vincent <vincent4502237@gmail.com>
2020-03-03 15:02:54 +0800

    添加 Apache License 2.0 开源协议

--- src/main/java/com/hpb/bc/util/apiKey/ApiName.java
+++ src/main/java/com/hpb/bc/util/apiKey/ApiName.java
@@ -1,18 +1,17 @@
 /*
- * Copyright 2007-2017 The Europeana Foundation
+ * Copyright 2020 HPB Foundation.
  *
- *  Licenced under the EUPL, Version 1.1 (the "Licence") and subsequent versions as approved
- *  by the European Commission;
- *  You may not use this work except in compliance with the Licence.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  You may obtain a copy of the Licence at:
- *  http://joinup.ec.europa.eu/software/page/eupl
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under
- *  the Licence is distributed on an "AS IS" basis, without warranties or conditions of
- *  any kind, either express or implied.
- *  See the Licence for the specific language governing permissions and limitations under
- *  the Licence.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 /**
